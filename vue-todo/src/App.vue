33498f0d1b697d0359e9dd39f79bb35c17df44e7
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-10-22 21:05:28 +0900

    style: 전역 css 적용

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -8,20 +8,37 @@
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue';
-import TodoList from './components/TodoList.vue';
-import TodoInput from './components/TodoInput.vue';
-import TodoFooter from './components/TodoFooter.vue';
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     TodoHeader,
     TodoList,
     TodoInput,
-    TodoFooter,
-  },
+    TodoFooter
+  }
 };
 </script>
 
-<style></style>
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+
+input {
+  border-style: groove;
+  widows: 200px;
+}
+
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
