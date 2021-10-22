56bcd2423a7f1a41bf33354b5c411e6d528da7ea
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-10-22 21:05:50 +0900

    feat: todo input 완료

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,11 +1,68 @@
 <template>
-  <div>
-    input
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fas fa-plus"></i>
+    </span>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    };
+  },
+  methods: {
+    addTodo: function() {
+      // 저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem = "";
+    }
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+  width: 90%;
+}
+
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+
+.addContainer:hover {
+  cursor: pointer;
+}
+
+.addContainer .fa-plus {
+  color: #fff;
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
